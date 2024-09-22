a6ad6c9beb90874cfda90c49834fae314db1bab1
Cyril Bouvart <75614845+CyrilBvt13@users.noreply.github.com>
2024-09-22 18:59:52 +0200

    Update PlayMusic.ino

--- Benchmark/PlayMusic.ino
+++ Benchmark/PlayMusic.ino
@@ -1,1 +1,16 @@
-aaa
+#include <DYPlayerArduino.h>
+
+// Créer une instance du lecteur DYPlayer
+DY::Player player;
+
+void setup() {
+
+  // Initialiser la communication série
+  player.begin();
+  delay(100);
+  player.setVolume(20); // 30 = 100% Volume
+  player.setCycleMode(DY::PlayMode::Sequence);
+
+  // Tester la carte son
+  player.play()
+}
